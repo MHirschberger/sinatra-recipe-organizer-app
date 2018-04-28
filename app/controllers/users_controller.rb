1a94ad743710f4244d4d6d4c241c83cf67ea1440
Michael Hirschberger <mlhirschberger@gmail.com>
2018-04-28 01:24:28 -0400

    Building out users controller and signup.erb file.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,52 @@
 class UsersController < ApplicationController
+  # get '/users/:id' do
+  #   @user = User.find(params[:id])
+  #   erb :'users/show'
+  # end
+
+  get '/signup' do
+    if !logged_in?
+      erb :'users/signup', locals: {message: "Please sign up before you sign in"}
+    else
+      redirect to '/welcome'
+    end
+  end
+
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect to '/signup'
+    else
+      @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      redirect to '/recipes'
+    end
+  end
+
+  get '/login' do
+    if !logged_in?
+      erb :'users/login'
+    else
+      redirect '/recipes'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/recipes"
+    else
+      redirect to '/signup'
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.destroy
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end
 end
