f49b275a98c06a407f2160894c10625a212f935a
Michael Hirschberger <mlhirschberger@gmail.com>
2018-04-27 20:48:46 -0400

    Create category index

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,7 @@
 class CategoriesController < ApplicationController
+
+  get '/categories' do
+    @categories = Category.all.sort_by {|category| category.name}
+    erb :'categories/index'
+  end
 end
