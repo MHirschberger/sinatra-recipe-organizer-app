72210198a697451df89c05eb2225a188b401d7cd
Michael Hirschberger <mlhirschberger@gmail.com>
2018-05-02 19:35:52 -0400

    Refactoring recipes_controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,114 +1,92 @@
 class RecipesController < ApplicationController
 
   get '/recipes' do
-    if logged_in?
-      @recipes = current_user.recipes.sort_by {|recipe| recipe.name}
-      erb :'recipes/index'
-    else
-      redirect to '/login'
-    end
+    redirect_if_not_logged_in
+    @recipes = current_user.recipes.sort_by {|recipe| recipe.name}
+    erb :'recipes/index'
   end
 
   get '/recipes/new' do
-    if logged_in?
-      erb :'recipes/new'
-    else
-      redirect to '/login'
-    end
+    redirect_if_not_logged_in
+    erb :'recipes/new'
   end
 
   post '/recipes' do
-    if logged_in?
-      if params[:recipe_name] == ""
-        redirect to "/recipes/new"
-      else
-        @recipe = current_user.recipes.build(name: params[:recipe_name], ingredients: params[:ingredients], instructions: params[:instructions])
-        if params[:new_category] == ""
-          @recipe.category_id = params[:category]
-        else
-          @recipe.category = Category.find_or_create_by(name: params[:new_category], user_id: current_user.id)
-        end
-        if @recipe.save
-          redirect to "/recipes/#{@recipe.id}"
-        else
-          redirect to "/recipes/new"
-        end
-      end
+    redirect_if_not_logged_in
+    if params[:recipe_name] == ""
+      redirect to "/recipes/new"
     else
-      redirect to '/login'
+      @recipe = current_user.recipes.build(name: params[:recipe_name], ingredients: params[:ingredients], instructions: params[:instructions])
+      if params[:new_category] == ""
+        @recipe.category_id = params[:category]
+      else
+        @recipe.category = Category.find_or_create_by(name: params[:new_category], user_id: current_user.id)
+      end
+      if @recipe.save
+        redirect to "/recipes/#{@recipe.id}"
+      else
+        redirect to "/recipes/new"
+      end
     end
   end
 
   get '/recipes/:id' do
-    if logged_in?
-      @recipe = Recipe.find(params[:id])
-      erb :'recipes/show'
-    else
-      redirect to '/login'
-    end
+    redirect_if_not_logged_in
+    @recipe = Recipe.find(params[:id])
+    erb :'recipes/show'
   end
 
   get '/recipes/:id/edit' do
-    if logged_in?
-      @recipe = Recipe.find(params[:id])
-      if @recipe && @recipe.category.user == current_user
-        erb :'recipes/edit'
-      else
-        redirect to '/recipes'
-      end
+    redirect_if_not_logged_in
+    @recipe = Recipe.find(params[:id])
+    if @recipe && @recipe.category.user == current_user
+      erb :'recipes/edit'
     else
-      redirect to '/login'
+      redirect to '/recipes'
     end
   end
 
   patch '/recipes/:id' do
-    if logged_in?
-      if params[:recipe_name] == ""
-        redirect to "/recipes/#{@recipe.id}/edit"
-      else
-        @recipe = current_user.recipes.find(params[:id])
-        if @recipe && @recipe.category.user == current_user
-          if @recipe.update(name: params[:recipe_name], ingredients: params[:ingredients], instructions: params[:instructions])
-            if params[:new_category] == ""
-              @recipe.category_id = params[:category]
-            else
-              @recipe.category = Category.find_or_create_by(name: params[:new_category], user_id: current_user.id)
-            end
-            @recipe.save
-            current_user.categories.each do |category|
-              if category.recipes.empty?
-                current_user.categories.delete(category)
-              end
-            end
-            redirect to "/recipes/#{@recipe.id}"
-          else
-            redirect to "/recipes/#{@recipe.id}/edit"
-          end
-        else
-          redirect to '/recipes'
-        end
-      end
+    redirect_if_not_logged_in
+    if params[:recipe_name] == ""
+      redirect to "/recipes/#{@recipe.id}/edit"
     else
-      redirect to '/login'
+      @recipe = current_user.recipes.find(params[:id])
+      if @recipe && @recipe.category.user == current_user
+        if @recipe.update(name: params[:recipe_name], ingredients: params[:ingredients], instructions: params[:instructions])
+          if params[:new_category] == ""
+            @recipe.category_id = params[:category]
+          else
+            @recipe.category = Category.find_or_create_by(name: params[:new_category], user_id: current_user.id)
+          end
+          @recipe.save
+          current_user.categories.each do |category|
+            if category.recipes.empty?
+              current_user.categories.delete(category)
+            end
+          end
+          redirect to "/recipes/#{@recipe.id}"
+        else
+          redirect to "/recipes/#{@recipe.id}/edit"
+        end
+      else
+        redirect to '/recipes'
+      end
     end
   end
 
   delete '/recipes/:id/delete' do
-    if logged_in?
-      @recipe = Recipe.find(params[:id])
-      if @recipe && @recipe.category.user == current_user
-        @recipe.delete
-        current_user.categories.each do |category|
-          if category.recipes.empty?
-            current_user.categories.delete(category)
-          end
+    redirect_if_not_logged_in
+    @recipe = Recipe.find(params[:id])
+    if @recipe && @recipe.category.user == current_user
+      @recipe.delete
+      current_user.categories.each do |category|
+        if category.recipes.empty?
+          current_user.categories.delete(category)
         end
       end
-      redirect to '/recipes'
-    else
-      redirect to '/login'
-
     end
+    redirect to '/recipes'
   end
 
 end
