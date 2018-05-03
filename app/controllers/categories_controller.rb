8e0473d7496937e798711440cd50c18953e89b4b
Michael Hirschberger <mlhirschberger@gmail.com>
2018-05-03 00:08:03 -0400

    Refactored categories controller.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,20 +1,19 @@
 class CategoriesController < ApplicationController
 
   get '/categories' do
-    if logged_in?
-      @categories = current_user.categories.sort_by {|category| category.name}
-      erb :'categories/index'
-    else
-      redirect to '/login'
-    end
+    redirect_if_not_logged_in
+    @categories = current_user.categories.sort_by {|category| category.name}
+    erb :'categories/index'
   end
 
   get '/categories/:id' do
-    if logged_in?
-      @category = Category.find(params[:id])
+    redirect_if_not_logged_in
+    @category = Category.find(params[:id])
+    if @category && @category.user == current_user
       erb :'categories/show'
     else
-      redirect to '/login'
+      flash[:notice] = "Invalid Request"
+      redirect to '/categories'
     end
   end
 
